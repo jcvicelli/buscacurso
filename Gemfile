fb0ee6216ed21812c50e95cc60ca1a3886926a43
jcvicelli <jcvicelli@gmail.com>
2016-01-01 12:31:39 -0200

    Devise with i18n pt-BR

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'puma'
 gem 'devise', '3.4.1'
 gem 'devise-i18n'
 gem 'bootstrap-sass'
+gem 'rails-i18n'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50}
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable,
          :lockable
