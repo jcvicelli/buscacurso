02819224ec4648ef7f61055c71cc548bf29a758b
jcvicelli <jcvicelli@gmail.com>
2016-03-20 19:40:59 -0300

    more on autocomplete

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:show, :index, :search]
+  before_action :authenticate_user!, except: [:show, :index, :search, :autocomplete]
   before_action :owned_course, only: [:edit, :update, :destroy]
 
   @areas = Area.all
