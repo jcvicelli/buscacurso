be98805bca8b51d077ca7763c1c693b17caae972
jcvicelli <jcvicelli@gmail.com>
2016-04-02 17:44:45 -0300

    on create course only let choose company that you are employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,4 +5,8 @@ class Employee < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
+
+  def company_name
+    "#{company.name}"
+  end
 end
